f4312e01b21512d5d723be7452a7c427c419e5c3
MauroDeveloper7 <duqueivan493@gmail.com>
2024-03-04 19:37:18 -0500

    Cambio en los colores del login y register, img, etc

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,10 +9,9 @@
         <div class="container">
             <div class="row justify-content-center">
                 <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
-
                     <div class="d-flex justify-content-center py-4">
-                        <a href="index.html" class="logo d-flex align-items-center w-auto">
-                            <img style="max-height: 60px" src="{{asset('img/logoba.png')}}" alt="">
+                        <a href="index.html" class="logo d-flex align-items-center w-auto">//Hacer que lo lleve a la pagina principla como canva
+                            <img style="max-height: 60px" src="{{asset('img/Bienestar-al-Aprendiz.png')}}" alt="">
                         </a>
                     </div><!-- End Logo -->
 
@@ -21,8 +20,8 @@
                         <div class="card-body">
 
                             <div class="pt-4 pb-2">
-                                <h5 class="card-title text-center pb-0 fs-4">Login to Your Account</h5>
-                                <p class="text-center small">Enter your username & password to login</p>
+                                <h5 class="card-title-ba text-center pb-0 fs-4">Ingrese a su cuenta</h5>
+                                <p class="text-center small">Ingrese su correo electrónico institucional y contraseña</p>
                             </div>
 
                             @include('compartido.alertas')
@@ -30,7 +29,7 @@
                             <form action="{{route('auth.login')}}" class="row g-3 needs-validation" novalidate method="POST">
                                 @csrf
                                 <div class="col-12">
-                                    <label for="yourUsername" class="form-label">Username</label>
+                                    <label for="yourUsername" class="form-label">Correo electrónico</label>
                                     <div class="input-group has-validation">
                                         <span class="input-group-text" id="inputGroupPrepend">@</span>
                                         <input value="{{old('email')}}" type="text" name="email" class="form-control {{$errors->has('email') ? 'is-invalid':''}}" id="yourUsername" required>
@@ -41,7 +40,7 @@
                                 </div>
 
                                 <div class="col-12">
-                                    <label for="yourPassword" class="form-label">Password</label>
+                                    <label for="yourPassword" class="form-label">Contraseña</label>
                                     <input value="{{old('password')}}" type="password" name="password" class="form-control {{$errors->has('password')?'is-invalid':''}}" id="yourPassword" required>
                                     @error('password')
                                     <li class="text-danger">{{ $message}}</li>
@@ -51,28 +50,18 @@
                                 <div class="col-12">
                                     <div class="form-check">
                                         <input class="form-check-input" type="checkbox" name="remember" value="true" id="rememberMe">
-                                        <label class="form-check-label" for="rememberMe">Remember me</label>
+                                        <label class="form-check-label" for="rememberMe">Recordarme</label>
                                     </div>
                                 </div>
                                 <div class="col-12">
-                                    <button class="btn btn-primary w-100" type="submit">Login</button>
+                                    <button class="btn btn-ba w-100" type="submit">Ingresar</button>
                                 </div>
                                 <div class="col-12">
-                                    <p class="small mb-0">Don't have account? <a href="{{route('auth.registerform')}}">Create an account</a></p>
+                                    <p class="small mb-0">No tiene cuenta? <a class="vinculos" href="{{route('auth.registerform')}}">Crear cuenta</a></p>
                                 </div>
                             </form>
-
                         </div>
                     </div>
-
-                    <div class="credits">
-                        <!-- All the links in the footer should remain intact. -->
-                        <!-- You can delete the links only if you purchased the pro version. -->
-                        <!-- Licensing information: https://bootstrapmade.com/license/ -->
-                        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ -->
-                        Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-                    </div>
-
                 </div>
             </div>
         </div>
