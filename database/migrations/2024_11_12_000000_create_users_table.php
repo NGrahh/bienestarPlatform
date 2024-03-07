fe4ba06551eae7082c2382255ebcd8e3e816f858
Mauricio <160339021+MauroDeveloper7@users.noreply.github.com>
2024-03-06 21:31:12 -0500

    Correcion migracion

--- database/migrations/2024_11_12_000000_create_users_table.php
+++ database/migrations/2024_11_12_000000_create_users_table.php
@@ -16,18 +16,19 @@ return new class extends Migration
             $table->string('name');
             $table->string('lastname');
             $table->string('user_name');
-            $table->string('type_rh_id');
             $table->string('email')->unique();
             $table->string('document')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->unsignedBigInteger('type_document_id');
             $table->unsignedBigInteger('rol_id');
+            $table->unsignedBigInteger('type_rh_id');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
             
             $table->foreign('type_document_id')->references('id')->on('type_documents');
             $table->foreign('rol_id')->references('id')->on('roles');
+            $table->foreign('type_rh_id')->references('id')->on('type_rhs');
         });
     }
 
