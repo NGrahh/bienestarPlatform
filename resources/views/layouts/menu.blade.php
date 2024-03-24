ee597bb2ee0466dbf921750d7ae8a159d0015f5e
MauroDeveloper7 <duqueivan493@gmail.com>
2024-03-24 18:01:03 -0500

    Cambios ubicacion de formularios

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -2,33 +2,45 @@
 <aside id="sidebar" class="sidebar">
 
   <ul class="sidebar-nav" id="sidebar-nav">
-
     <li class="nav-item">
-      <a class="nav-link " href="{{route('home')}}">
+      <a class="nav-link collapsed" href="{{route('home')}}">
         <i class="bi bi-grid"></i>
         <span>Inicio</span>
       </a>
     </li>
 
-    @if(session('rol_id') == 1)
-      @include('layouts.menus.menu-admin')
+    @if(session('rol_id') == 0) {{-- Administrador --}}
       @include('layouts.menus.menu-usuarios')
+    @endif  
+
+    @if(session('rol_id') == 1) {{-- Administrador --}}
+      @include('layouts.menus.menu-admin')
     @endif
 
-    {{-- @if(session('rol_id') == 2)
-      @include()
+    @if(session('rol_id') == 2) {{-- Lider Bienestar --}}
+      @include('layouts.menus.menu-lider-bienestar')
     @endif
 
-    @if(session('rol_id') == 3)
-      @include()
+    @if(session('rol_id') == 3) {{-- Miembro Bienestar --}}
+      @include('layouts.menus.menu-miembroBA')
     @endif
 
-    @if(session('rol_id') == 4)
-      @include()
+    @if(session('rol_id') == 4) {{-- Miembro Bienestar apoyo --}}
+      @include('layouts.menus.menu-miembro-BA-apoyo')
     @endif
 
-    @if(session('rol_id') == 5)
-      @include()
-    @endif --}}
+    @if(session('rol_id') == 5) {{-- Aprendiz --}}
+      @include('layouts.menus.menu-aprendiz')
+    @endif
+
+    <li class="nav-item">
+      <a class="nav-link collapsed" href="{{route('home')}}">
+        <i class="ri-body-scan-fill"></i>
+        <span>Quienes somos?</span>
+      </a>
+    </li>
+
+
   </ul>
+
 </aside>
--- resources/views/layouts/menus/menu-usuarios.blade.php
+++ resources/views/layouts/menus/menu-usuarios.blade.php
@@ -1,51 +1,90 @@
-<li class="nav-item">
-    <a class="nav-link collapsed" data-bs-target="#events-nav" data-bs-toggle="collapse" href="#">
-        <i class="bi bi-layout-text-window-reverse"></i><span>Eventos</span><i class="bi bi-chevron-down ms-auto"></i>
-    </a>
-    <ul id="events-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
-        <li>
-            <a href="#">
-                <i class="bi bi-circle"></i><span>Registrar</span>
-            </a>
-        </li>
-        <li>
-            <a href="#">
-                <i class="bi bi-circle"></i><span>Listado</span>
-            </a>
-        </li>
-    </ul>
-</li><!-- End Tables Nav -->
-<li class="nav-item">
-    <a class="nav-link collapsed" data-bs-target="#" data-bs-toggle="collapse" href="#">
-        <i class="bi bi-layout-text-window-reverse"></i><span>Servicios</span><i class="bi bi-chevron-down ms-auto"></i>
-    </a>
-    <ul id="events-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
-        <li>
-            <a href="#">
-                <i class="bi bi-circle"></i><span>------</span>
-            </a>
-        </li>
-        <li>
-            <a href="#">
-                <i class="bi bi-circle"></i><span>------</span>
-            </a>
-        </li>
-    </ul>
-</li><!-- End Tables Nav -->
-<li class="nav-item">
-    <a class="nav-link collapsed" data-bs-target="#" data-bs-toggle="collapse" href="#">
-        <i class="bi bi-layout-text-window-reverse"></i><span>Apoyos</span><i class="bi bi-chevron-down ms-auto"></i>
-    </a>
-    <ul id="events-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
-        <li>
-            <a href="#">
-                <i class="bi bi-circle"></i><span>------</span>
-            </a>
-        </li>
-        <li>
-            <a href="#">
-                <i class="bi bi-circle"></i><span>------</span>
-            </a>
-        </li>
-    </ul>
-</li>
+<ul class="sidebar-nav" id="sidebar-nav">
+    <li class="nav-item">
+        <a class="nav-link collapsed" data-bs-target="#events-nav" data-bs-toggle="collapse" href="#">
+            <i class="bi bi-layout-text-window-reverse"></i><span>Eventos</span><i class="bi bi-chevron-down ms-auto"></i>
+        </a>
+        <ul id="events-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+            <li>
+                <a href="#">
+                    <i class="bi bi-circle"></i>Eventos Proximos<span></span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="bi bi-circle"></i><span>Eventos Pasados</span>
+                </a>
+            </li>
+        </ul>
+    </li><!-- End Tables Nav -->
+    <li class="nav-item">
+        <a class="nav-link collapsed" data-bs-target="#services-nav" data-bs-toggle="collapse" href="#">
+            <i class="bi bi-layout-text-window-reverse"></i><span>Servicios</span><i class="bi bi-chevron-down ms-auto"></i>
+        </a>
+        <ul id="events-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+            <li>
+                <a href="#">
+                    <i class="bi bi-circle"></i>-----<span></span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="bi bi-circle"></i><span>----</span>
+                </a>
+            </li>
+        </ul>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link collapsed" data-bs-target="#supports-nav" data-bs-toggle="collapse" href="#">
+            <i class="bi bi-layout-text-window-reverse"></i><span>Apoyos</span><i class="bi bi-chevron-down ms-auto"></i>
+        </a>
+        <ul id="events-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+            <li>
+                <a href="#">
+                    <i class="bi bi-circle"></i>Apoyo de sostenimiento FIC<span></span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="bi bi-circle"></i><span>Apoyo de sostenimiento regular</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="bi bi-circle"></i><span>Apoyos de transporte</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="bi bi-circle"></i><span>Apoyo de alimentación</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="bi bi-circle"></i><span>Centros de convivencia</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="bi bi-circle"></i><span>Plan de datos</span>
+                </a>
+            </li>
+        </ul>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link collapsed" data-bs-target="#dimensions-nav" data-bs-toggle="collapse" href="#">
+            <i class="bi bi-layout-text-window-reverse"></i><span>Dimensiones de bienestar al aprendiz</span><i class="bi bi-chevron-down ms-auto"></i>
+        </a>
+        <ul id="events-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+            <li>
+                <a href="#">
+                    <i class="bi bi-circle"></i>-----<span></span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <i class="bi bi-circle"></i><span>----</span>
+                </a>
+            </li>
+        </ul>
+    </li>
+</ul>
