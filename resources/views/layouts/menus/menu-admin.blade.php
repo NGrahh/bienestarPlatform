8af219fe93fd8f82865f243d0aeb1300c69bc1a4
Sebas1596 <sebastianfernanndez9@gmail.com>
2024-04-14 11:24:39 -0500

    Menu Admin

--- resources/views/layouts/menus/menu-admin.blade.php
+++ resources/views/layouts/menus/menu-admin.blade.php
@@ -6,18 +6,106 @@
   <ul id="events-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
     <li>
       <a href="{{route('eventos.create')}}">
-        <i class="bi bi-circle"></i><span>Registrar</span>
+        <i class="bi bi-circle"></i><span>Agregar evento</span>
       </a>
     </li>
     <li>
       <a href="{{route('eventos.index')}}">
-        <i class="bi bi-circle"></i><span>Listado</span>
+        <i class="bi bi-circle"></i><span>Listado de eventos</span>
+      </a>
+    </li>
+    <li>
+      <a href="">
+        <i class="bi bi-circle"></i><span>Evento proximo</span>
+      </a>
+    </li>
+    <li>
+      <a href="">
+        <i class="bi bi-circle"></i><span>Evento pasado</span>
       </a>
     </li>
   </ul>
   
 </li><!-- End Tables Nav -->
+
+<li class="nav-item">
+  <a class="nav-link collapsed" data-bs-target="#supports-nav" data-bs-toggle="collapse" href="#">
+      <i class="ri-pantone-line"></i><span>Apoyos</span><i class="bi bi-chevron-down ms-auto"></i>
+  </a>
+  <ul id="supports-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+      <li>
+          <a href="{{route('Apoyo-fic')}}">
+              <i class="bi bi-circle"></i>Apoyo de sostenimiento FIC<span></span>
+          </a>
+      </li>
+      <li>
+          <a href="{{route('Apoyo-regular')}}">
+              <i class="bi bi-circle"></i><span>Apoyo de sostenimiento regular</span>
+          </a>
+      </li>
+      <li>
+          <a href="{{route('Apoyo-transporte')}}">
+              <i class="bi bi-circle"></i><span>Apoyos de transporte</span>
+          </a>
+      </li>
+      <li>
+          <a href="{{route('Apoyo-alimentacion')}}">
+              <i class="bi bi-circle"></i><span>Apoyo de alimentación</span>
+          </a>
+      </li>
+      <li>
+          <a href="#">
+              <i class="bi bi-circle"></i><span>Centros de convivencia</span>
+          </a>
+      </li>
+      <li>
+          <a href="{{route('Apoyo-datos')}}">
+              <i class="bi bi-circle"></i><span>Plan de datos</span>
+          </a>
+      </li>
+      <li>
+        <a href="">
+          <i class="bi bi-circle"></i><span>Ver inscritos</span>
+        </a>
+      </li>
+  </ul>
+</li>
   
-  
-  
-  
+<li class="nav-item">
+  <a class="nav-link collapsed" data-bs-target="#dimensions-nav" data-bs-toggle="collapse" href="#">
+      <i class="ri-psychotherapy-line"></i><span>Dimensiones de bienestar al aprendiz</span><i class="bi bi-chevron-down ms-auto"></i>
+  </a>
+  <ul id="dimensions-nav" class="nav-content collapse " data-bs-parent="#sidebar-nav">
+      <li>
+          <a href="#">
+              <i class="bi bi-circle"></i>Consejería y atención psicosocial<span></span>
+          </a>
+      </li>
+      <li>
+          <a href="#">
+              <i class="bi bi-circle"></i><span>Arte y cultura</span>
+          </a>
+      </li>
+      <li>
+          <a href="#">
+              <i class="bi bi-circle"></i><span>Deportes</span>
+          </a>
+      </li>
+      <li>
+          <a href="#">
+              <i class="bi bi-circle"></i><span>Promoción y prevención de salud</span>
+          </a>
+      </li>
+      <li>
+          <a href="#">
+              <i class="bi bi-circle"></i><span>Apoyos de sostenimiento</span>
+          </a>
+      </li>
+      <li>
+          <a href="#">
+              <i class="bi bi-circle"></i><span>Liderazgo</span>
+          </a>
+      </li>
+  </ul>
+</li>
+
