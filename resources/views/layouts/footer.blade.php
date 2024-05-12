e9e6150428aa682984c20f36805a42849f7d7c99
NGrahh <144406323+NGrahh@users.noreply.github.com>
2024-05-12 14:58:45 -0500

    Limpieza

--- resources/views/layouts/footer.blade.php
+++
@@ -1,7 +0,0 @@
-<footer id="footer" class="footer">
-    <div class="copyright"> <strong><span>Servicio Nacional de Aprendizaje</span></strong>
-    </div>
-    <div class="credits">
-        Centro de Diseño e Innovación Tecnológica Industrial- <a href="https://bootstrapmade.com/">Regional Risaralda</a>
-    </div>
-</footer><!-- End Footer -->
