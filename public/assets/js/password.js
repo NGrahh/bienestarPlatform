b6f64d89b88005932417eb3f48dbb7cbbd5044b4
Mauricio <duqueivan493@gmail.com>
2024-08-05 18:57:52 -0500

    Login 4.1

--- public/assets/js/password.js
+++ public/assets/js/password.js
@@ -1,7 +1,12 @@
-$('#togglePassword').on('touchstart', function () {
+$('#togglePassword').on('click', function () {
     var $passwordField = $('#yourPassword');
     var type = $passwordField.attr('type') === 'password' ? 'text' : 'password';
     $passwordField.attr('type', type);
-    $(this).removeClass('bi-eye-fill');
-    $(this).addClass('bi-eye-slash-fill');
+    if ($(this).hasClass('bi-eye-fill')) {
+        $(this).removeClass('bi-eye-fill');
+        $(this).addClass('bi-eye-slash-fill');
+    } else {
+        $(this).removeClass('bi-eye-slash-fill');
+        $(this).addClass('bi-eye-fill');
+    }
 });
